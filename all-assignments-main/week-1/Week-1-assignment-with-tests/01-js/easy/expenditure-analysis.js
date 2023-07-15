6bc342f1ec91a5ef049549eb44f8477c71ab8345
cmk <contact@chandan.fyi>
2023-07-15 16:52:17 -0400

    solved-01-easy-expenditure

--- all-assignments-main/week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ all-assignments-main/week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -9,7 +9,65 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  //  return [];
+
+  var summary = {}; // empty object
+  for (var i = 0; i < transactions.length; i++) {
+    var t = transactions[i];
+    if (summary[t.category]) {
+      summary[t.category] = summary[t.category] + t.price
+    } else {
+      summary[t.category] = t.price
+    }
+  }
+  //console.log(summary)
+  // since we need return type as array we need to push it to array 
+  // Converting object to array ----
+  var onlyKey = Object.keys(summary)
+  console.log(summary);
+  console.log(onlyKey);
+
+  let answer = []
+  for (var i = 0; i < onlyKey.length; i++) {
+    var cat = onlyKey[i];
+    // var obj = {
+    //   category: cat,
+    //   total_amount_Spent: summary[cat]
+    // }
+    // answer.push(obj)
+    answer.push({
+      category: cat,
+      totalSpent: summary[cat]
+    })
+
+  }
+  console.log(answer)
+  //---------------------------------------------------------
+  return answer
 }
+
+// disbabled for testing
+// var transactions = [
+//   {
+//     item: "pepsi",
+//     category: "drink",
+//     price: 20,
+//     timestamp: "12-may-1203"
+//   },
+//   {
+//     item: "cola",
+//     category: "drink",
+//     price: 25,
+//     timestamp: "13-may-1203"
+//   },
+//   {
+//     item: "Cake",
+//     category: "food",
+//     price: 4,
+//     timestamp: "13-may-1203"
+//   }
+// ]
+
+// var f = calculateTotalSpentByCategory(transactions)
 
 module.exports = calculateTotalSpentByCategory;
