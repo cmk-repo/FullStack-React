3ff36574fe4acaadc552b76a2f48f83751c326f4
cmk <contact@chandan.fyi>
2023-07-15 17:24:03 -0400

    solved-01-med-palindrom

--- all-assignments-main/week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ all-assignments-main/week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -6,8 +6,40 @@
   - `npm run test-palindrome`
 */
 
+// take care of special charecters such as space comma dot etc 
+function transform_(str) {
+  let b = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " " || str[i] === "," || str[i] === "?" || str[i] === "!" || str[i] === ".") {
+
+    } else {
+      b += str[i]
+    }
+  }
+  return b
+}
+
+function reverse(str) {
+  let a = "";
+  //console.log(str.length)
+  for (let i = str.length - 1; i >= 0; i--) {
+    a += str[i]
+  }
+  return a
+}
+
+
 function isPalindrome(str) {
-  return true;
+  // return true;
+  str = str.toLowerCase();
+  // clear extra stuff first 
+  str = transform_(str);
+  let rstr = reverse(str);
+  if (rstr === str) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = isPalindrome;
