e37a1473bdf2f40e1a339ab451572401badded9b
cmk-repo <contact@chandan.fyi>
2023-08-05 12:53:33 -0400

    setInterval in state management

---
+++ vite-react-dom-state-management/src/App copy 2.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+
+function App() {
+  const [todoForToday , setTodoForToday] = React.useState([{
+    title: "Title 1",
+    description: " des 1 ",
+    id: 1
+  },
+  {
+    title: " Hello 2",
+    description: " Des 2 ",
+    id: 2
+  }]);
+
+
+//1
+  return (
+    <div>
+     {JSON.stringify(todoForToday)}
+     <br />
+     {todoForToday[0].title}
+     <br />
+     {todoForToday[1].title}
+     <br />
+     {todoForToday.map((todo)=>{
+      return (
+        <div>
+          {todo.title}
+          {todo.description}
+          <br />
+        </div>
+      )
+     })}
+    {todoForToday.map((todo)=>{
+      return (<ComponentTodo title={todo.title} description={todo.description}></ComponentTodo>)
+    })}
+    </div>
+  )
+}
+
+function ComponentTodo(props){
+  return <div style={{backgroundColor : "red"}}>
+    {props.title}
+    {props.description}
+  </div>
+}
+export default App;
