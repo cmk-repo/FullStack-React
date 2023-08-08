200f95baa3c1f471063e38ae6c955ab6383179d1
cmk-repo <contact@chandan.fyi>
2023-08-07 22:26:50 -0400

    week5.1 Start

---
+++ vite-project-week5-1-courseweb/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Signup from './Signup.jsx'
+
+
+function App() {
+  return (
+    <>
+      <Signup></Signup>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project-week5-1-courseweb/src/Signup.jsx
@@ -0,0 +1,10 @@
+function Signup() {
+    return <div>
+        username -<input> type = {"text"} </input>
+        <br/>
+        password -<input> type = {"password"} </input>
+        <br/>
+        <button>sign up</button>
+    </div>
+}
+export default Signup
