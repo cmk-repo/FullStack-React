cff2e5301e11afcbc7e2329aaae22f9b375347d9
Chandan <tochandan@icloud.com>
2023-08-08 15:42:36 -0400

    signup-Mui

--- vite-project-week5-1-courseweb/src/Signup.jsx
+++ vite-project-week5-1-courseweb/src/Signup.jsx
@@ -1,10 +1,32 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import { Typography } from '@mui/material';
+
 function Signup() {
     return <div>
-        username -<input> type = {"text"} </input>
+        <center>
+            <div style={{
+                marginTop: 150,
+                marginBottom: 10
+            }}>
+                <Typography variant="h6"> Welcome to DataBird</Typography>
+            </div>
+          </center>
+        <center>
+        <Card variant="outlined" style = {{width:400,padding:20}}>
+        <TextField fullWidth={true} id="email" label="Email" variant="outlined" />
+        <br/>
+        <TextField  fullWidth={true} id="pass" label="Password" variant="outlined" />
+        <br/>
+        <Button variant="contained">Sign up</Button>
+        {/* username -<input> type = {"text"} </input>
         <br/>
         password -<input> type = {"password"} </input>
         <br/>
-        <button>sign up</button>
+        <button>sign up</button> */}
+     </Card>
+        </center>
     </div>
 }
 export default Signup
